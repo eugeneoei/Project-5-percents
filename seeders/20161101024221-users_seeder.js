1a42c63b524a6c34d30d513c8436d362f2635962
Eugene Oei <eugeneoei89@gmail.com>
2016-11-01 11:54:30 +0800

    edited seed files

--- seeders/20161101024221-users_seeder.js
+++ seeders/20161101024221-users_seeder.js
@@ -8,10 +8,20 @@ module.exports = {
             last_name: 'Tan',
             email: 'peter@email.com',
             is_admin: true,
-            password: 'peter@email.com',
+            password: '$2a$10$niihHBliuSrUgONneVEMzeGFjxwh2LDABNmA5Fr/1Mh7YdtJ8cQn2',
             createdAt: new Date(),
             updatedAt: new Date()
         },
+
+        {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john@email.com',
+            is_admin: false,
+            password: '$2a$10$WgPlFfeBpSVBDYWIFeSF6O4VEz/U8E9Atq8X0TM1SC1ZoJIZrbaJG',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
     ], {});
   },
 
