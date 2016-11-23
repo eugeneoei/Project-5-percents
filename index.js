25698c011b4f64019c1c7a0f4641f1e08e4d1dad
Eugene Oei <eugeneoei89@gmail.com>
2016-11-23 15:06:49 +0800

    test for bug fixed

--- index.js
+++ index.js
@@ -141,29 +141,6 @@ app.get('/home', function (req, res) {
   // console.log('this is result', result);
 });
 
-
-// test /home route
-// app.get('/home', function (req, res) {
-//   var result = [];
-//   db.drop.findAll({
-//     order: [['id', 'ASC']]
-//   }, function(drops) {
-//     results.push(drops);
-//     db.poll.findAll({
-//       include: [db.option],
-//       order: [ [db.option, 'votes', 'DESC'] ]
-//     }, function(polls) {
-//       result.push(polls);
-//       db.user.find({
-//         where: {id: req.user.id}
-//       }).then(function(user) {
-//         res.render('user', {result:result, user:user})
-//       })
-//     })
-//   })
-// });
-
-
 // get all drops
 // this is working
 // app.get('/drops', function(req,res) {
